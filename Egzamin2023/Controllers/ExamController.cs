7fcca5989565c55505b1966cd7daa4428978f019
michalszura <160254740+randomUserMala@users.noreply.github.com>
2024-02-16 16:57:33 +0100

    ZADANIE 1, widok formularz, model note, controller examcontroller

---
+++ Egzamin2023/Controllers/ExamController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Egzamin2023.Models;
+
+
+namespace Egzamin2023.Controllers
+{
+    public class ExamController : Controller
+    {
+        [HttpGet]
+        public IActionResult Create() // pomieniamy "View() na Create(), tworzymy ścieżkę "Exam/Create.cshtml" w Views.
+        {
+            return View();
+        }
+    }
+}
+
---
+++ Egzamin2023/Models/Note.cs
@@ -0,0 +1,25 @@
+﻿// ZADANIE 1. MODEL NOTE, KLASA, WIDOK, CONTROLLER
+
+using System.ComponentModel.DataAnnotations; // dodajemy DataAnnotations aby móc używać [display], [key] itp.
+namespace Egzamin2023.Models
+{
+	public class Note
+	{
+		[Key] // "identyfikator" 
+		[MinLength(3)] // minimalna długość znaków
+		[MaxLength(20)] // maksymalna długość znaków
+        [Display(Name = "Tytuł")] // wyświetlana nazwa
+        public string Title { get; set; }
+
+
+        [MinLength(10)] // minimalna długość znaków
+        [MaxLength(2000)] // maksymalna długość znaków
+        [Display(Name = "Treść")] // wyświetlana nazwa
+        public string Content { get; set; }
+
+
+        [Display(Name = "Data ważności")] // wyświetlana nazwa
+        public DateTime Deadline { get; set; }
+	}
+}
+
